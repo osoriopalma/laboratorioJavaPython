360d3e227005d3b7c2cf7ef8f1167c2b4383cc75
Osorio <27744674+osoriopalma@users.noreply.github.com>
2026-05-20 21:29:48 -0600

    Fase 1,paso 1, implementación de singleton de la clase parkConfig lo cual permitirá leer el archivo park.properties una sola vez para que todas las clases compartan la misma lectura.

--- src/main/java/com/axity/dinosaurpark/Main.java
+++ src/main/java/com/axity/dinosaurpark/Main.java
@@ -1,7 +1,14 @@
 package com.axity.dinosaurpark;
 
+import com.axity.dinosaurpark.config.ParkConfig;
+
 public class Main {
-public static void main(String[] args) {
-    
-}
+
+    public static void main(String[] args) {
+
+        ParkConfig config = ParkConfig.getInstance();
+
+        System.out.println("Seed: " + config.getSeed());
+        System.out.println("Total Steps: " + config.getTotalSteps());
+    }
 }
---
+++ src/main/java/com/axity/dinosaurpark/config/ParkConfig.java
@@ -0,0 +1,52 @@
+package com.axity.dinosaurpark.config;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ParkConfig {
+    private static ParkConfig instance;
+    private final Properties properties;
+
+    private ParkConfig() {
+        properties = new Properties();
+        // Cargamos el archivo park.properties, en caso de que se genere una excepsion
+        // se termina la ejecucuin de la palicación
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("park.properties")) {
+            if (input == null) {
+                throw new RuntimeException("No se encontró park.properties");
+            }
+            properties.load(input);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // implementamos sigleton para que todos los clases trabajen con el mismo objeto
+    // de configuración
+    public static ParkConfig getInstance() {
+        if (instance == null) {
+            instance = new ParkConfig();
+        }
+        return instance;
+    }
+
+    public int getInt(String key, int defaultValue) {
+        return Integer.parseInt(properties.getProperty(key, String.valueOf(defaultValue)));
+    }
+
+    public double getDouble(String key, double defaultValue) {
+        return Double.parseDouble(properties.getProperty(key, String.valueOf(defaultValue)));
+    }
+
+    public String getString(String key, String defaultValue) {
+        return properties.getProperty(key, defaultValue);
+    }
+
+    public long getSeed() {
+        return getInt("simulation.seed", 42);
+    }
+
+    public int getTotalSteps(){
+        return getInt("Simulation.totalSteps", 100);
+    }
+}
